facdf6c42bc6e1d276e09815a582c7d060f96d46
VickyKao <vickykao53@hotmail.com>
2017-12-01 03:36:54 +0800

    加入簽名板專案。

---
+++ XFParcelSign/XFParcelSign/ViewModels/SignaturePadPageViewModel.cs
@@ -0,0 +1,47 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using SignaturePad.Forms;
+using XFParcelSign.Views;
+
+namespace XFParcelSign.ViewModels
+{
+
+    public class SignaturePadPageViewModel : INotifyPropertyChanged, INavigationAware {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private readonly INavigationService _navigationService;
+
+        public DelegateCommand SubmitCommand { get; set; }
+
+
+
+        public SignaturePadPageViewModel(INavigationService navigationService) {
+            _navigationService = navigationService;
+
+            SubmitCommand = new DelegateCommand(async () => {
+                //var signedImageStream = await signPad.GetImageStreamAsync(SignatureImageFormat.Png);
+                //if (signedImageStream != null) {
+                //}
+            });
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters) {
+
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters) {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters) {
+
+        }
+
+    }
+
+}
---
+++ XFParcelSign/XFParcelSign/Views/SignaturePadPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xamarin.Forms;
+using SignaturePad.Forms;
+
+namespace XFParcelSign.Views
+{
+    public partial class SignaturePadPage : ContentPage
+    {
+        public SignaturePadPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
